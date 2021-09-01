46e4f9e780cdb62792a315e1bd4e230557fd8908
Profe Ema <girolamiem@gmail.com>
2021-09-01 11:21:24 -0300

    sacamos los include

--- arraylist.c
+++ arraylist.c
@@ -1,8 +1,8 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
 
-#include "arraylist.h"
+// #include "arraylist.h"
 
 /*-----------------------------------
   The Array List Data Structure
